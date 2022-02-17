9614106372a43b7269c3ab327ed92cf5541c0e45
tomthomas1 <tomthomas8688@gmail.com>
2022-02-18 02:00:07 +0530

    Monthly Wages

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,20 +2,28 @@ package com.bridgelabz;
 
 public class EmployeeWage {
 	public static void main(String[] args) {
-		int attendance = (int) Math.floor((Math.random() * 3));
+
 		int wages = 20;
+		int Monthly_Working_Days = 20;
+		int totalhrs=0;
+		
+		for(int i=0; i<=20; i++) {
+		int attendance = (int) Math.floor((Math.random() * 3));
 		switch (attendance) {
 		case 0:
 			int empHrs = 4;
-			System.out.println("Employee is Present and done Part Time: \n Wage is " + empHrs * wages);
+			System.out.println("Employee is Present and done Part Time");
+			 totalhrs += empHrs;
 			break;
 		case 1:
 			int empHr = 8;
-			System.out.println(" Employee is Present and done Full Time: \n Wage is " + empHr * wages);
+			System.out.println("Employee is Present and done Full Time");
+		      totalhrs += empHr;
 			break;
 		case 2:
-			System.out.println(" Employee is absent \n Wage is 0 ");
+			System.out.println("Employee is Absent ");
 			break;
-		}
+		}  
+		} System.out.println(" \n The Monthly Wage is : " + totalhrs*wages);
 	}
 }
