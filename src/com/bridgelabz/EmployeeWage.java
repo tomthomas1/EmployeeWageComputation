913a41d1eec17ddc2f7cab69ef50a9da5016ceed
tomthomas1 <tomthomas8688@gmail.com>
2022-02-24 00:24:04 +0530

    Compute Employee wage for multiple companies

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,48 +3,57 @@ package com.bridgelabz;
 public class EmployeeWage {
 	private static final int IS_FULL_TIME = 1;
 	private static final int IS_PART_TIME = 2;
-
-	// method to compute employee wage
-	public static int calculateEmpWageForCompany(String company, int empRate, int numOFDays, int maxHours) {
-
-		System.out.println("\n ----- Welcome to Employee Wage Computation! -----");
-		int empHrs = 0, empWagePerMonth = 0, totalWorkingdays = 0, totalEmpHours = 0;
-
-		while (totalEmpHours <= maxHours && totalWorkingdays < numOFDays) { // condition
-
-			totalWorkingdays++;
-			int empType = (int) Math.floor(Math.random() * 10) % 3; // random fun to check if employee is doing full
-																	// time or part time
-
-			switch (empType) {
+	
+	private final String company;
+	private final int empRate;
+	private final int numOFDays;
+	private final int maxHours;
+	
+	public EmployeeWage(String company, int empRate, int numOFDays, int maxHours) {                //creating constructor for input of data
+		this.company = company;
+		this.empRate = empRate;
+		this.numOFDays = numOFDays;
+		this.maxHours = maxHours;
+	}
+	public int calculateEmpWageForCompany() {
+		
+		System.out.println("----- Welcome to Employee Wage Computation! -----");
+		int empHrs=0, empWagePerMonth = 0, totalWorkingdays=0, totalEmpHours = 0;
+				
+		while(totalEmpHours <= this.maxHours && totalWorkingdays < this.numOFDays) {
+			
+			totalWorkingdays ++;
+			int empType = (int)Math.floor(Math.random() * 10) % 3;
+			
+			switch(empType) {
 			case IS_FULL_TIME:
 				empHrs = 8;
-				System.out.println("Employee has done a Full Time");
 				break;
 			case IS_PART_TIME:
 				empHrs = 4;
-				System.out.println("Employee has done a Part Time");
 				break;
 			default:
 				empHrs = 0;
-				System.out.println("Employee was Absent");
 			}
-			// to calculate the employee wage
+			
 			totalEmpHours += empHrs;
-			int empWagePerDay = empHrs * empRate;
+			int empWagePerDay = empHrs * this.empRate;
 			empWagePerMonth += empWagePerDay;
-			System.out.println("Emp Wage Per Day : " + empWagePerDay);
+			System.out.println("Emp Wage Per Day : "+empWagePerDay);
 		}
-
-		System.out.println("Emp Wage Per Month for Company " + company + " is Rs." + empWagePerMonth);
-		return empWagePerMonth; // to return the wage
+		
+		System.out.println("Emp Wage Per Month for Company "+this.company+" is Rs."+empWagePerMonth);
+		return empWagePerMonth;
 	}
-
-	public static void main(String[] args) {
-		// calling the method with parameter input
-		calculateEmpWageForCompany("TATA", 30, 3, 30);
-		calculateEmpWageForCompany("Reliance", 20, 4, 40);
-
+	
+	public static void main(String[] args) {                                                    // we are passing the values to EmployeeWage and then 
+		EmployeeWage tata = new EmployeeWage("TATA", 20, 2, 10);                                //  calculating the wage for all company. We have created the object of the company
+		EmployeeWage reliance = new EmployeeWage("RELIANCE", 40, 5, 20);
+		tata.calculateEmpWageForCompany();
+		System.out.println();
+		reliance.calculateEmpWageForCompany();
+		
+	
 	}
 
 }
